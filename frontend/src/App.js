acc92f29afe7a27d11748149c2109e6d22fdfe0f
f1x3d49 <davidnicoara28@gmail.com>
2023-02-25 19:11:35 +0200

    started building the component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
+import photo from "./images/image-qr-code.png";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    <div className="bg-design_lgrey w-screen h-screen">
+      <div className="bg-red w-100 h-96">
+        <img className="w-64" src={photo} alt="QR Code" />
+      </div>
     </div>
   );
 }
